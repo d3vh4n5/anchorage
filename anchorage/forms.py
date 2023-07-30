9ee7f3607c71b2317e740cd12a2b075378e5a2e0
Juan <juanangelbasgall@hotmail.com>
2023-07-30 00:44:22 -0300

    Se avanzó mucho sobre los formularios de login y registro, y emarejé el proyecto a la clase 17

--- anchorage/forms.py
+++ anchorage/forms.py
@@ -1,6 +1,41 @@
 from django import forms
+from django.core import validators
+from django.forms import ValidationError
+
+def largo_validate(value):
+    print(value)
+    if len(value) >10 or len(value)< 8:
+        raise ValidationError("Debe tener entre 8 y 10 caracteres", code="error_largo",)
 
 class SignUp(forms.Form):
-    nombre = forms.CharField(label='Nombre de usuario:', required=True)
-    email = forms.EmailField(label='e-mail: ', required=True)
-    password = forms.CharField(label='Contraseña:', widget=forms.PasswordInput, required=True)
+    SEX_CHOICES = (
+        (1, "Masculino"),
+        (2, "Femenino"),
+        (3, "Otro.."),
+    )
+
+    nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Nombre de usuario:', required=True)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), label='e-mail: ', required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Contraseña:', required=True, validators=(largo_validate, ))
+    sexo = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-select'}),label='Sexo: ', choices=SEX_CHOICES)
+    nacimiento = forms.DateField(
+        label='Fecha de nacimiento:',
+        widget=forms.SelectDateWidget(
+            years=range(1920, 2023),
+            attrs={'class': 'form-select'}
+        )
+    )
+    terms = forms.BooleanField(
+        label='He leído y acepto los términos de uso ', 
+        required=True, 
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+class Login(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control mt-2 mb-2'}), required=True)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control mt-2 mb-2'}), label='Contraseña:', required=True, validators=(largo_validate, ))
+    noRobot = forms.BooleanField(
+        label='No soy un robot.', 
+        required=True, 
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input mt-2 mb-2'})
+    )
--- anchorage/views.py
+++ anchorage/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,7 +21,18 @@ def index(request):
     return render(request, 'anchorage/index.html', context)
 
 def login(request):
-    return render(request, 'anchorage/pages/login.html')
+    if request.method == 'POST':
+        login_form = Login(request.POST)
+        if login_form.is_valid():
+            # redirigir al user page
+            print('Login correcto')
+        else:
+            print("Error de autenticación")
+            messages.error(request, "Usuario o contraseña incorrectos")
+    else:
+        login_form = Login()
+    context = {'login_form' : login_form}
+    return render(request, 'anchorage/pages/login.html', context)
 
 
 def userpage(request):
@@ -38,6 +50,12 @@ def userpage(request):
 def signup(request):
     if request.method == "POST":
         signup_form = SignUp(request.POST)
+        if signup_form.is_valid():
+            messages.success(request, "Formulario cargado con éxito")
+            print("Todo joya")
+        else:
+            messages.error(request, "La contraseña debe tener entre 8 y 10 caracteres")
+            print("Todo mal")
     else:
         signup_form = SignUp()
     context = {'signup_form': signup_form}
